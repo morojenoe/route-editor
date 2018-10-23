32ee0891ced6c21fc8d54b7dee80c3e647d7f047
Dmitry Sutygin <dmitry.sutygin@gmail.com>
2018-10-23 08:15:27 +0400

    Updates

---
+++ precache-manifest.d9c7b39ad0c3326a07901c20b11429a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "541730b4074aa44d15d3",
+    "url": "/static/js/main.541730b4.chunk.js"
+  },
+  {
+    "revision": "98b383254ae607cdc5ac",
+    "url": "/static/js/1.98b38325.chunk.js"
+  },
+  {
+    "revision": "541730b4074aa44d15d3",
+    "url": "/static/css/main.fc7c7304.chunk.css"
+  },
+  {
+    "revision": "a254cc07059a21f195cc5b5c273a1e8b",
+    "url": "/index.html"
+  }
+];
